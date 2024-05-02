6e78963cd2760bfd3cd75a1f458645075f65437b
Lalit Pagare <lalitpagarerealme@gmail.com>
2024-05-02 16:42:05 +0530

    starPattern 3

---
+++ BasicsOfJava/Startp3.java
@@ -0,0 +1,18 @@
+package BasicsOfJava;
+
+public class Startp3 {
+    public static void main(String[] args) {
+        for (int row = 1; row <= 4; row++) { // Outer loop
+
+            for (int sp = 1; sp <= 4 - row; sp++) { // Inner loop
+                System.out.print(" ");
+            }
+
+            for (int col = 1; col <= row; col++) { // Inner loop
+                System.out.print(row + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
