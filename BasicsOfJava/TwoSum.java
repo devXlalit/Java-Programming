a856b1550bf21276a75bde1556d48e18104094a4
Lalit Pagare <lalitpagarerealme@gmail.com>
2024-05-06 16:47:14 +0530

    duptotal

--- BasicsOfJava/TwoSum.java
+++ BasicsOfJava/TwoSum.java
@@ -15,5 +15,11 @@ public class TwoSum {
     }
 }
 
-// Assignment - Q.Write a program in java to check wheather an interget in
+// Assignments
+// Q1.Write a program in java to check wheather an interget in
 // duplicate or not if integet in duplicate print true else print false
+
+// Q2.Write a program in java to print the total number of
+// target value occurance in an array
+// Explanation-[1,2,3,4,1,1] target = 1
+// output - total number of occurance of 1 is 3
